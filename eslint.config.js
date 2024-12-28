8350010b13c28fd0376984c44387e103f7ab0a72
taiga1071101 <taigatukimi@gmail.com>
2024-12-29 00:23:30 +0900

    Station2までクリア

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// import globals, { browser } from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  {
+    languageOptions: {
+      globals: {
+        //globals.browser
+        browser: true,
+        node: true,
+        jest: true,
+        console: true,
+        document: true,
+        process: true, //
+        test: true, // test環境で使用されるグローバル変数。これをESLintに認識されることで、余計なエラーを解消させる
+        expect: true,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: 'detect', // Reactのバージョンを自動検出
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // React 17以降ではスコープにReactをインポートしなくてもOK
+    },
+  },
+];
--- src/const.js
+++ src/const.js
@@ -1,1 +1,2 @@
+// export const url = "https://railway.todo.techtrain.dev";
 export const url = process.env.REACT_APP_API_URL;
